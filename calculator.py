10ac8cd21e5593b6277ea6947392ead4dee6cb6d
DrinkPesticide <adamlkrogers@yahoo.com>
2024-11-20 12:08:18 +0000

    coommmits

--- calculator.py
+++ calculator.py
@@ -1,4 +1,19 @@
-def cube(a):
-    b = a * a * a 
-    return b 
+# basic calculator functions library,
+# if there was a main() function for running the calculator 
+# if __name__ == "__main__": is used to allow for testing functionality
+def addition(a, b):
+    c = (a+b)
+    return c 
+
+def subtraction(a, b):
+    c = (a-b)
+    return c 
+
+def multiplication(a, b):
+    c = (a*b)
+    return c   
+
+def division(a, b):
+    c = (a/b)
+    return c
 
---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+from calculator import addition
+# import pytest
+
+def main():
+    assert addition(5, 7) == 12
+
+if __name__ == "__main__":
+    main() 
